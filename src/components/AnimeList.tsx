3f4d178ab890fe14a288cb8617bad1ce7afcd8f6
lolOzzi <47964440+lolOzzi@users.noreply.github.com>
2023-04-25 23:33:33 +0200

    animelist ranking changes

--- src/components/AnimeList.tsx
+++ src/components/AnimeList.tsx
@@ -4,14 +4,6 @@ import { clientOnly } from 'solid-start/islands';
 import { unstable_clientOnly } from 'solid-start';
 import "./AnimeList.css";
 
-//  https://docs.api.jikan.moe/ for at finde overkatagorier, underkatagorier og parametre
-//  syntax: getAnimeList("Overkatagorie", "Underkatagorie", {parametre})  
-//  ex. getAnimeList("top", "getTopAnime", { page: 1 }) 
-//  ex. getAnimeList("top", "getTopAnime")  //  default page = 1, så dette er det samme som ovenstående
-//  ex. getAnimeList("anime", "getAnimeSearch", { q: "naruto", page: 1 })
-/*export const theThing = clientOnly<any>(async () => { 
-  return {default: await getAnimeList("top", "getTopAnime", { page: 1 })}
-})*/
 export const getAnimeList = async <T extends keyof JikanClient>(objectName: T, methodName: keyof JikanClient[T], ...args: any[]) => {
 
   const jikanClient = new JikanClient();
@@ -40,43 +32,7 @@ export type AnimeShow = {
 
 export default function AnimeList(props: any) {
   const [local, others] = splitProps(props, ['animeList', 'isUserList', 'isRanked']);
-  if (local.isUserList) {
-    return (
-      <>
-        <h2>My Anime List</h2>
-        <table class="anime-table">
-          <tbody>
-            <tr>
-              <th>
-                <b>#</b>
-              </th>
-              <th>
-                <b>Image</b>
-              </th>
-              <th>
-                <b>Title</b>
-              </th>
-              <th>
-                <b>Your Rating</b>
-              </th>
-            </tr>
-            <For each={local.animeList}>
-              {(anime, i) => (
-                <tr class="animelist-item-container">
-                  <Show when={local.isRanked || local.isUserList}>
-                    {i() + 1}
-                  </Show>
-                  <td><img src={anime.image_url} alt={anime.title} /></td>
-                  <td><p>{anime.title}</p></td>
-                  <td>{anime.rating}</td>
-                </tr>
-              )}
-            </For>
-          </tbody>
-        </table>
-      </>
-    );
-  }
+
   return (
     <>
       <table class="anime-table">
@@ -93,21 +49,26 @@ export default function AnimeList(props: any) {
             <th>
               <b>Title</b>
             </th>
-            <th>
-              <b>Score</b>
-            </th>
+            <Show when={local.isUserList}
+              fallback={<th><b>Score</b></th>}>
+              <th><b>Rating</b></th>
+            </Show>
           </tr>
           <For each={local.animeList}>
             {(anime, i) => (
               <tr class="animelist-item-container">
                 <Show when={local.isRanked || local.isUserList}>
-                  <p>{i() + 1}</p>
+                  <td class="ranking" >
+                        {i() + 1}
+                    </td>
                 </Show>
                 <td><img src={anime.image_url} alt={anime.title} /></td>
                 <td><p>{anime.title}</p></td>
-                <td>{anime.score}</td>
+                <Show when={local.isUserList}
+                  fallback={<td>{anime.score}</td>}>
+                  <td>{anime.rating}</td>
+                </Show>
               </tr>
-
             )}
           </For>
         </tbody>
